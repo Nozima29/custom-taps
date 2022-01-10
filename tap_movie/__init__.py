932fdd65b2f7caf50cd72d39ae63b6b1f7a7678a
Nozima29 <nozima.9813@gmail.com>
2022-01-11 01:34:04 +0500

    update

--- tap_movie/__init__.py
+++ tap_movie/__init__.py
@@ -21,11 +21,11 @@ def get_abs_path(path):
 def load_schemas():
     """Load schemas from schemas folder"""
     schemas = {}
-    # for filename in os.listdir(get_abs_path("schemas")):
-    path = get_abs_path("schemas") + "/movie.json"  # + filename
-    # file_raw = filename.replace(".json", "")
-    with open(path) as file:
-        schemas[path] = Schema.from_dict(json.load(file))
+    for filename in os.listdir(get_abs_path("schemas")):
+        path = get_abs_path("schemas/") + filename
+        file_raw = filename.replace(".json", "")
+        with open(path) as file:
+            schemas[file_raw] = Schema.from_dict(json.load(file))
     return schemas
 
 
@@ -33,7 +33,17 @@ def discover():
     raw_schemas = load_schemas()
     streams = []
     for stream_id, schema in raw_schemas.items():
-        stream_metadata = []
+        stream_metadata = [
+            {
+                "breadcrumb": [],
+                "metadata": {
+                    "table-key-properties": ["id"],
+                    "selected": "true",
+                    "forced-replication-method": "INCREMENTAL",
+                    "valid-replication-keys": ["generated_timestamp"],
+                },
+            }
+        ]
         key_properties = []
         streams.append(
             CatalogEntry(
@@ -57,7 +67,6 @@ def discover():
 def sync(config, state, catalog):
     """Sync data from tap source"""
     # Loop over selected streams in catalog
-
     for stream in catalog.get_selected_streams(state):
 
         LOGGER.info("Syncing stream:" + stream.tap_stream_id)
@@ -71,8 +80,9 @@ def sync(config, state, catalog):
             key_properties=stream.key_properties,
         )
 
-        client = MovieConnect(API_KEY, end_id=120)
+        client = MovieConnect(API_KEY, end_id=105)
         tap_data = client.fetch_data()
+
         max_bookmark = None
         for row in tap_data:
             singer.write_records(stream.tap_stream_id, [row])
@@ -81,6 +91,7 @@ def sync(config, state, catalog):
                     singer.write_state({stream.tap_stream_id: row[bookmark_column]})
                 else:
                     max_bookmark = max(max_bookmark, row[bookmark_column])
+
         if bookmark_column and not is_sorted:
             singer.write_state({stream.tap_stream_id: max_bookmark})
     return
