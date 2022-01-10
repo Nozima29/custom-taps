912fcd7ac52d47aec1b4a6810ddc87e7584aa291
Nozima29 <nozima.9813@gmail.com>
2022-01-11 00:21:30 +0500

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# tap-movie
+
+This is a [Singer](https://singer.io) tap that produces JSON-formatted data
+following the [Singer
+spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
+
+
+Running Tap:
+
+    python3 -m venv ~/.virtualenvs/tap-movie
+    source ~/.virtualenvs/tap-movie/bin/activate
+    pip install tap-movie
+    
+    tap-movie -c config.json --catalog catalog.json --discover (for doscover mode)
+
+    or
+
+    tap-movie -c config.json --catalog catalog.json
+    (for sync mode)
+
+
+
---
+++ tap_movie/connect.py
@@ -0,0 +1,29 @@
+import requests
+import singer
+
+LOGGER = singer.get_logger()
+
+
+class MovieConnect:
+    def __init__(self, key, end_id, start_id=100):
+        self.start_id = start_id
+        self.end_id = end_id
+        self.key = key
+
+    def fetch_data(self):
+        data = []
+        id = self.start_id
+        while id < self.end_id:
+            url = "https://api.themoviedb.org/3/movie/{}?api_key={}".format(
+                id, self.key
+            )
+            try:
+                client = requests.get(url)
+                LOGGER.info("Fetching data from {}".format(url))
+                # json_object = json.dumps(client.json(), indent=4)
+                data.append(client.json())
+                id += 1
+            except:
+                LOGGER.info("Error in connecting to {}".format(url))
+
+        return data
